d8cfb4b7d8499633c1c73481f65cdb61658148b0
Tristan Whitaker <tristanrileyw@gmail.com>
2015-05-21 12:19:27 -0700

    Created LandingPage. added menu and project links

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/home.css"> 
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<title>Tristan Whitaker</title>
+</head>
+<body>
+	<div class="title">
+		<h1>Tristan Whitaker</h1>	
+	</div>
+<div class="content">
+  <a id="menu-button"></a>
+  <div id="hamburger-menu">
+    <nav>
+      <a class="links" href="http://localhost/landingpage/home.php">Home</a>
+      <a class="links" href="">About</a>
+      <a class="links" href="http://localhost/landingpage/index.php">Works</a>
+      <a class="links" href="">Contact</a>
+    </nav>
+  </div>
+  <div id="overlay"></div>
+</div>
+
+</body>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</html>
